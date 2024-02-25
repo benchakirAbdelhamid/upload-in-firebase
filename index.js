80462593efe5d89b807ebac19e723a9d434394a1
hamid <benchakirhamid@gmail.com>
2024-02-25 17:14:06 +0100

    update methode upload pdf

--- index.js
+++ index.js
@@ -1,193 +1,14 @@
-// const express = require('express')
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const app = express()
-// require("dotenv").config();
-
-// const multer = require('multer')
-// const firebase = require('firebase/app')
-// const {getStorage , ref , uploadBytes ,getDownloadURL} = require('firebase/storage')
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAHWXg4IaYbcP5GFvMsCpRBINowYbWPlNY",
-//   authDomain: "firbaseupload-875a4.firebaseapp.com",
-//   projectId: "firbaseupload-875a4",
-//   storageBucket: "firbaseupload-875a4.appspot.com",
-//   messagingSenderId: "857578320530",
-//   appId: "1:857578320530:web:1bec2b8ce154fd0bad6c4b"
-// };
-// firebase.initializeApp(firebaseConfig)
-// const storage = getStorage()
-
-// const upload = multer({Storage : multer.memoryStorage()})
-
-// //Middlewares ==>json body
-// app.use(bodyParser.json());
-
-// async function startServer() {
-//     // try {
-//     //   await mongoose.connect(process.env.DATABASE, { dbName: "DBimages" });
-//     //   console.log("Successfully connected to MongoDB");
-//     // } catch (err) {
-//     //   console.error("Failed to connect to MongoDB:", err);
-//     //   return;
-//     // }
-
-//     const port = process.env.PORT || 5005;
-//     app.listen(port, () => console.log(`App is running on port ${port}`));
-//   }
-
-//   startServer();
-
-// app.get('/',(req , res)=>{
-//     res.json('Firebase storage in cloud')
-// })
-
-// app.post('/',upload.single('filename'),async(req, res)=>{
-//   // const storageRef = ref(storage,req.file.originalname);
-//   const storageRef = ref(storage,`files/${req.file.originalname}`);
-
-//   uploadBytes(storageRef , req.file.buffer).then(async(snapshot)=>{
-//     const imageURL = await getDownloadURL(storageRef);
-//     console.log('file uploaded')
-//     console.log('Image URL:', imageURL);
-//   })
-
-//     // console.log('===> ',req.file)
-// })
-
-// // // ===============stock image in firebase and show code binary image=================
-
-// const express = require('express')
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const app = express()
-// require("dotenv").config();
-
-// const multer = require('multer')
-// const firebase = require('firebase/app')
-// const {getStorage , uploadBytesResumable , ref , uploadBytes ,getDownloadURL } = require('firebase/storage')
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBwZXW6-8xzDNrzKLT3e_wPBY6-Qw2-Cpc",
-//   authDomain: "upload-files-7e3b2.firebaseapp.com",
-//   projectId: "upload-files-7e3b2",
-//   storageBucket: "upload-files-7e3b2.appspot.com",
-//   messagingSenderId: "1018741061383",
-//   appId: "1:1018741061383:web:0f2f80e12a0178a3acf7a2"
-// };
-// firebase.initializeApp(firebaseConfig)
-// const storage = getStorage()
-
-// const upload = multer({Storage : multer.memoryStorage()})
-
-// //Middlewares ==>json body
-// app.use(bodyParser.json());
-
-// async function startServer() {
-//     // try {
-//     //   await mongoose.connect(process.env.DATABASE, { dbName: "DBimages" });
-//     //   console.log("Successfully connected to MongoDB");
-//     // } catch (err) {
-//     //   console.error("Failed to connect to MongoDB:", err);
-//     //   return;
-//     // }
-
-//     const port = process.env.PORT || 5005;
-//     app.listen(port, () => console.log(`App is running on port ${port}`));
-//   }
-
-//   startServer();
-
-// app.get('/',(req , res)=>{
-//     res.json('Firebase storage in cloud')
-// })
-
-// app.post('/',upload.single('filename'),async(req, res)=>{
-//   try {
-//     const infoimg = req.file
-//     const storageRef = ref(storage, `files/${req.file.originalname}`);
-//     await uploadBytes(storageRef, req.file.buffer);
-//     const imageURL = await getDownloadURL(storageRef);
-//     res.json({ message : 'File uploaded' ,
-//     imageURL ,
-//      infoimg  });
-//   } catch (error) {
-//     console.error('Error uploading file to Firebase Storage:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// })
-
-// ==========================================
-
-// // ======1=========stock image in firebase and show code binary image=================
-
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const app = express();
-// require("dotenv").config();
-
-// const multer = require("multer");
-// const firebase = require("firebase/app");
-// const {
-//   getStorage,
-//   uploadBytesResumable,
-//   ref,
-//   uploadBytes,
-//   getDownloadURL,
-// } = require("firebase/storage");
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBwZXW6-8xzDNrzKLT3e_wPBY6-Qw2-Cpc",
-//   authDomain: "upload-files-7e3b2.firebaseapp.com",
-//   projectId: "upload-files-7e3b2",
-//   storageBucket: "upload-files-7e3b2.appspot.com",
-//   messagingSenderId: "1018741061383",
-//   appId: "1:1018741061383:web:0f2f80e12a0178a3acf7a2",
-// };
-// firebase.initializeApp(firebaseConfig);
-// const storage = getStorage();
-
-// const upload = multer({ Storage: multer.memoryStorage() });
-
-// //Middlewares ==>json body
-// app.use(bodyParser.json());
-
-// async function startServer() {
-//   const port = process.env.PORT || 5005;
-//   app.listen(port, () => console.log(`App is running on port ${port}`));
-// }
-
-// startServer();
-
-// app.get("/", (req, res) => {
-//   res.json("Firebase storage in cloud");
-// });
-
-// app.post("/", upload.single("filename"), async (req, res) => {
-//   try {
-//     const infoimg = req.file;
-//     const storageRef = ref(storage, `images/${req.file.originalname}`);
-//     await uploadBytes(storageRef, req.file.buffer);
-//     const imageURL = await getDownloadURL(storageRef);
-//     res.json({ message: "File uploaded", imageURL, infoimg });
-//   } catch (error) {
-//     console.error("Error uploading file to Firebase Storage:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// vedio youtube 1 = nice upload img in firebase and get url image from firebase
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require('cors')
+
 const app = express();
 require("dotenv").config();
+//Middlewares ==>json body
+app.use(express.json())
+// Middleware cors == link between back and front
+app.use(cors())
 
 const multer = require("multer");
 const firebase = require("firebase/app");
@@ -217,6 +38,13 @@ const upload = multer({ Storage: multer.memoryStorage() });
 app.use(bodyParser.json());
 
 async function startServer() {
+  //     // try {
+//     //   await mongoose.connect(process.env.DATABASE, { dbName: "DBimages" });
+//     //   console.log("Successfully connected to MongoDB");
+//     // } catch (err) {
+//     //   console.error("Failed to connect to MongoDB:", err);
+//     //   return;
+//     // }
   const port = process.env.PORT || 5005;
   app.listen(port, () => console.log(`App is running on port ${port}`));
 }
@@ -229,31 +57,34 @@ app.get("/", (req, res) => {
 const storage = getStorage();
 
 // upload img in firebase
-app.post("/", upload.single("filename"), async (req, res) => {
+app.post("/", upload.single("photo"), async (req, res) => {
   try {
     const dateTime = giveCurrentDateTime();
     const fileName = `${req.file.originalname + "_" + dateTime}`;
     const storageRef = ref(storage, `images/${fileName}`);
-    // Create the metadata including the content type
+    // // Create the metadata including the content type
     const metadata = {
       contentType: req.file.mimetype,
     };
-    await uploadBytesResumable(storageRef, req.file.buffer, metadata).then(
-      () => {
-        getDownloadURL(storageRef)
-          .then((url) => {
-            res.send({
-              message: "Image successfully uploaded",
-              url,
-              fileName: fileName,
-              img_info: req.file,
+    await uploadBytesResumable(storageRef, req.file.buffer, metadata)
+    .then(
+        () => {
+          getDownloadURL(storageRef)
+            .then((url) => {
+              res.send({
+                message: "Image successfully uploaded",
+                url,
+                fileName: fileName,
+                img_info: req.file,
+                req : req.body,
+              });
+            })
+            .catch((err) => {
+              res.status(500).send(err);
             });
-          })
-          .catch((err) => {
-            res.status(500).send(err);
-          });
-      }
-    );
+        }
+      );
+  
   } catch (error) {
     console.error("Error uploading file to Firebase Storage:", error);
     res.status(500).json({ error: "Internal Server Error" });
